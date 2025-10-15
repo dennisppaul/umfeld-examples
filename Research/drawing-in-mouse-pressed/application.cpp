ce7f99803e35027cd0e00ca61e185b360aafe12e
dennisppaul <dennis.paul@hfk-bremen.de>
2025-10-15 22:43:58 +0200

    added example

---
+++ Research/drawing-in-mouse-pressed/application.cpp
@@ -0,0 +1,28 @@
+/* this example shows how to draw in the 'setup()' function as well as in event functions ( e.g 'mousePressed()' ) */
+
+#include "Umfeld.h"
+
+using namespace umfeld;
+
+void settings() {
+    size(1024, 768);
+}
+
+void setup() {
+    noFill();
+    stroke(1.0f, 0.25f, 0.35f);
+    background(0.85f);
+    rectMode(CENTER);
+    rect(width / 2, height / 2, 200, 200);
+}
+
+void draw() {
+    // background(0.85f);
+    stroke(1.0, 0.0, 0.0);
+    line(width, height, mouseX, mouseY);
+}
+
+void mouseMoved() {
+    stroke(0.0, 0.0, 1.0);
+    line(0, 0, mouseX, mouseY);
+}
